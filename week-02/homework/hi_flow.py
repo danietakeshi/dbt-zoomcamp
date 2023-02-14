f9299dc83be00a161fe9535383dd671ea1be73e5
Daniel Takeshi <martins.takeshi@gmail.com>
2023-02-14 08:57:32 -0300

    Initial commit

--- week-02/homework/hi_flow.py
+++
@@ -1,9 +0,0 @@
-from prefect import flow, task
-@task
-def speak(message: str):
-    print(message)
-@flow(log_prints=True, name="prefect-example-hello-flow")
-def hi():
-    speak(message="Hi from Prefect! 🤗")
-if __name__ == "__main__":
-    hi()
